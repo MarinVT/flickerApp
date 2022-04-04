81a270ddb0a12f40c4aa915bd9a67ad2779bde05
Marin Tenzherski <marin_mm@abv.bg>
2022-04-04 23:30:29 +0300

    Added layouts and custom style: Textviews and ImageView

---
+++ app/src/main/java/flickerapp/com/PhotoDetailActivity.java
@@ -0,0 +1,43 @@
+package flickerapp.com;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import flickerapp.com.databinding.ActivityPhotoDetailBinding;
+
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityPhotoDetailBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityPhotoDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+}
